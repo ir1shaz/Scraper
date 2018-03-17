39e202f5dd032fe11c959a368b25f80ff7da3115
Andy Sargent <Andyjohnsargent@gmail.com>
2018-03-16 21:14:34 -0700

    add news.js

---
+++ models/News.js
@@ -0,0 +1,33 @@
+// Require mongoose
+var mongoose = require("mongoose");
+_____________________________________________
+// Create Schema class
+var Schema = mongoose.Schema;
+_____________________________________________
+
+// Create News schema
+var Newschema = new Schema({
+  // title is a required string
+  title: {
+    type: String,
+    required: true
+  },
+  // link is a required string
+  link: {
+    type: String,
+    required: true
+  },
+  // This only saves one note's ObjectId, ref refers to the Note model
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+_____________________________________________
+
+// Create the News model with the Newschema
+var News = mongoose.model("News", Newschema);
+
+
+// Export the model
+module.exports = News;
