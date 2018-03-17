7e61cec2200cd56aa9b9b14697c99bb974c4cc3c
Andy Sargent <Andyjohnsargent@gmail.com>
2018-03-16 21:12:21 -0700

    add note.js

---
+++ models/Note.js
@@ -0,0 +1,26 @@
+// Require mongoose
+var mongoose = require("mongoose");
+_____________________________________________
+
+// Create a schema class
+var Schema = mongoose.Schema;
+_____________________________________________
+
+// Create the Note schema
+var NoteSchema = new Schema({
+  // Just a string
+  title: {
+    type: String
+  },
+  // Just a string
+  body: {
+    type: String
+  }
+});
+_____________________________________________
+
+// Create the Note model with the NoteSchema
+var Note = mongoose.model("Note", NoteSchema);
+
+// Export the Note model
+module.exports = Note;
